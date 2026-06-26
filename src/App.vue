198240acd0bf4c738ac31be05d4e5c3b6a461351
IDCard Security Fix <security@pathumthani.police.go.th>
2026-06-26 15:48:18 +0000

    feat: wire up App.vue routing and clean boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { state } from './store.js'
+import RecipeSelector from './components/RecipeSelector.vue'
+import PrepChecklist from './components/PrepChecklist.vue'
+import LiveGuide from './components/LiveGuide.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <div class="min-h-screen bg-gray-200 font-sans text-gray-900 flex items-center justify-center p-4">
+    <div class="w-full max-w-4xl bg-white shadow-2xl rounded-2xl overflow-hidden min-h-[600px] flex">
+      <RecipeSelector v-if="state.currentStep === 'selection'" class="w-full" />
+      <PrepChecklist v-else-if="state.currentStep === 'prep'" class="w-full" />
+      <LiveGuide v-else-if="state.currentStep === 'live'" class="w-full" />
+    </div>
+  </div>
 </template>
